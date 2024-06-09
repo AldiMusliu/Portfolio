c993fbf7ed4d1e7c9d9ede10e7ff46e2ddee2fc1
aldimusliu <aldimusliu5@gmail.com>
2024-06-09 14:28:39 +0200

    changes in about me

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,24 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import profileImage from "@/public/images/profileImage.png";
 
 const About = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleMouseEnter = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  };
+
   return (
     <section
       id="about"
@@ -11,18 +26,28 @@ const About = () => {
     >
       <div className="container mx-auto flex flex-col md:flex-row items-center gap">
         <div className="w-full md:w-1/2 flex justify-center">
-          <div className="w-[450px] h-[450px] relative">
+          <div
+            className="w-[450px] h-[450px] relative rounded-full overflow-hidden group"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             <Image
               src={profileImage}
               alt="Profile"
-              className="rounded-full"
+              className="moving-image"
               layout="fill"
               objectFit="cover"
             />
+            <div className="audio-indicator">
+              <div className="w-2 h-2 bg-white animate-ping"></div>
+              <div className="w-2 h-2 bg-white animate-ping ml-2"></div>
+              <div className="w-2 h-2 bg-white animate-ping ml-2"></div>
+            </div>
           </div>
+          <audio ref={audioRef} src="/assets/audio.mp3" />
         </div>
         <div className="w-full md:w-1/2 mt-8 md:mt-0 md:ml-12">
-          <h2 className="text-4xl font-bold mb-4">About Me</h2>
+          <h2 className="text-5xl font-bold mb-4">About Me</h2>
           <p className="text-lg mb-4 text-gray-500">
             A small river named Duden flows by their place and supplies it with
             the necessary regelialia.
@@ -39,7 +64,7 @@ const About = () => {
             <div className="flex flex-col gap-2 text-gray-500">
               <p>Aldi Musliu</p>
               <p>July 20, 2001</p>
-              <p>Prishtine Kosova</p>
+              <p>Drenas, Kosova</p>
               <p>13000</p>
               <p>aldimusliu5@gmail.com</p>
               <p>+383 49 123 123</p>
@@ -50,9 +75,6 @@ const About = () => {
             <p className="text-[#ffbd39] text-3xl font-bold">5</p>
             <p className="text-lg">Project complete</p>
           </div>
-          <button className="mt-8 bg-[#ffbd39] text-black px-6 py-3 rounded-full font-bold">
-            DOWNLOAD CV
-          </button>
         </div>
       </div>
     </section>
